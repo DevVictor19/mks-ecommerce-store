8ccfe4cfb6da29247c2eb52304b7b9e69c6e06d1
Antonio Victor <Antoniovictor12@live.com>
2023-01-28 09:49:29 -0300

    chore(jest): adicionar config para imports

--- jest.config.ts
+++ jest.config.ts
@@ -3,5 +3,9 @@ export default {
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
+  moduleNameMapper: {
+    "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__mocks__/fileMock.js",
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+  },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
